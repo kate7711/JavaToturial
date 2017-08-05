5d21c4ef298ed23b92d61c9276e5d7d0e03ecb57
kate7711 <mayday5273@hotmail.com>
2017-08-05 11:37:15 +1000

    This is my first time

---
+++ Test.java
@@ -0,0 +1,10 @@
+class Test {
+	public static void main(String[] args) {
+		String name = "Alma";
+		System.out.println(name);
+//		 name = ""
+		int num;
+		num = 19880406;
+		System.out.println(num);
+	}
+}
